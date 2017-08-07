feabe76e7b32f996438e8b870f738c21d6a74a89
Daniel Garcia and Nate McGregor <daniel@>
2017-08-07 15:48:42 -0700

    updating javascript

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@
 */
 var sentence = prompt("Please type a sentence");
 alert(sentence);
-
+/*
 function capFirst(string)
 {
    sentence = sentence.charAt(0).toUpperCase() + sentence.slice(1);
@@ -32,5 +32,38 @@ function capLast(string)
 
 
 alert(capLast(sentence));
+*/
+function charAtEnd(string)
+{
+	 var last = sentence.charAt(sentence.length -1).toUpperCase();
+   var first = sentence.charAt(0).toUpperCase();
 
-alert(sentence);
+   return sentence +last + first ;
+};
+
+alert(charAtEnd(sentence));
+
+function midLet(string)
+{
+	 var last = sentence.charAt(sentence.length -1).toUpperCase();
+   var first = sentence.charAt(0).toUpperCase();
+   var index = sentence.charAt(Math.floor((sentence.length) / 2));
+
+
+
+   return index + sentence +last + first ;
+};
+
+alert(midLet(sentence));
+
+
+function reverse(string)
+{
+	 var last = sentence.charAt(sentence.length -1).toUpperCase();
+   var first = sentence.charAt(0).toUpperCase();
+   sentence = sentence.substring(1, sentence.length-1);
+
+   return last + sentence + first ;
+};
+
+alert(reverse(sentence));
